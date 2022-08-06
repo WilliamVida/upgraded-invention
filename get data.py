473f38eb6e7ec528be339688e020465d5d33c10c
WILLIAM VIDA <williamvidaisgod@gmail.com>
2022-08-06 19:20:20 +0300

    Can now get table data from website.

--- get data.py
+++ get data.py
@@ -1,1 +1,14 @@
-{\rtf1}
+import pandas as pd
+
+url = "https://www.johnstonsarchive.net/policy/abortion/usa/ab-usa-AL.html"
+
+table = pd.read_html(url)
+
+table = table[0]
+
+table = table[["year", "live births","abortionrate,residents", "abortion rate, merged"]]
+table.columns = ['Year', 'Live births','Abortion rate', 'Abortion rate (Guttmacher)']
+
+table = table.iloc[:-2]
+
+table.to_csv("Alabama.csv", index=False)
